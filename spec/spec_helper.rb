8e77f9843832c1ea6e22db1ddac271dbdb4f44b0
ksilin <konstantin.silin@gmail.com>
2014-07-27 21:33:30 +0200

    minor test improvements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
 require_relative '../app/server'
-# require 'rspec'
 require 'rack/test'
 require 'bundler/setup'
-
 require 'capybara/rspec'
 
 ENV['RACK_ENV'] = 'test'
+
 Sinatra::Application.environment = :test
 
 Bundler.require :default, Sinatra::Application.environment
@@ -20,6 +19,5 @@ RSpec.configure { |c|
   c.include Capybara::DSL#, feature: true
   c.include Capybara::RSpecMatchers#, feature: true
 }
-
 
 Capybara.app = Sinatra::Application
